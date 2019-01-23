b69938b64cd2c76cf9f817cefae4401b6c26bfb0
Ezra <deleguo@gmail.com>
2019-01-24 00:30:44 +0100

    Tile: improve visual representation, so as to print 3 tiles horizontally

--- src/spectranglegame/Tile.java
+++ src/spectranglegame/Tile.java
@@ -24,7 +24,6 @@ public class Tile {
 		rotation = 0;
 	}
 	
-	
 	/*
 	 * A public constructor that builds Tile with rotation 0;
 	 */
@@ -98,6 +97,7 @@ public class Tile {
      *  Function that rotates a tile with the tile given as a parameter
 	 * @param tile indicates the tile that will be rotated 
 	 */
+	// I would suggest not to rotate tile in place. 
 	public void rotateTile() {
 		if (this.getRotation() % 2 == 0) {
 			char v = getVertical();
@@ -129,15 +129,14 @@ public class Tile {
 		
 		if (this.isFacingUp()) {
 			// constructor order (t.val, t.vertical, t.left, t.right, t.rotation)
-			// when this.isFacingUp, color changes as follows: 
+			// when this.isFacingUp, rotate once, color changes as follows: 
 			// this.left     -->  t.vertical
 			// this.vertical -->  t.left
 			// this.right    -->  t.right
 			t = new Tile(value, left, vertical, right, 
 					     (rotation + 1) % 6);
 		} else {
-			// constructor order (t.val, t.vertical, t.left, t.right, t.rotation)
-			// when !this.isFacingUp, color changes as follows: 
+			// when !this.isFacingUp, rotate once, color changes as follows: 
 			// this.left     -->  t.left
 			// this.vertical -->  t.right
 			// this.right    -->  t.vertical
@@ -147,13 +146,55 @@ public class Tile {
 		return t;
 	}
 	
+	/**
+	 * @return A new Tile generated from this tile, after rotate twice.
+	 */
 	public Tile rotateTileTwice() {
-		// when rotating twice, no matter original is facing up or down:
-		// this.right    -->  t.vertical
-		// this.vertical -->  t.left
-		// this.left     -->  t.right
-		Tile t = new Tile(value, right, vertical, left, 
-						  (rotation + 2) % 6);
+		Tile t;
+		
+		if (this.isFacingUp()) {
+			// constructor order (t.val, t.vertical, t.left, t.right, t.rotation)
+			// when this.isFacingUp, rotate twice, color changes as follows: 
+			// this.left     -->  t.right
+			// this.vertical -->  t.left
+			// this.right    -->  t.vertical
+			t = new Tile(value, right, vertical, left, 
+					     (rotation + 2) % 6);
+		} else {
+			// constructor order (t.val, t.vertical, t.left, t.right, t.rotation)
+			// when !this.isFacingUp, rotate twice, color changes as follows: 
+			// this.left     -->  t.vertical
+			// this.vertical -->  t.right
+			// this.right    -->  t.left
+			t = new Tile(value, left, right, vertical, 
+						 (rotation + 2) % 6);
+		}
+		return t;
+	}
+	
+	/**
+	 * @return A new Tile generated from this tile, after rotate four times.
+	 */
+	public Tile rotateTileFourTimes() {
+		Tile t;
+		
+		if (this.isFacingUp()) {
+			// constructor order (t.val, t.vertical, t.left, t.right, t.rotation)
+			// when this.isFacingUp, rotate four times, color changes as follows: 
+			// this.left     -->  t.vertical
+			// this.vertical -->  t.right
+			// this.right    -->  t.left
+			t = new Tile(value, left, right, vertical, 
+					     (rotation + 4) % 6);
+		} else {
+			// constructor order (t.val, t.vertical, t.left, t.right, t.rotation)
+			// when !this.isFacingUp, rotate four times, color changes as follows: 
+			// this.left     -->  t.right
+			// this.vertical -->  t.left
+			// this.right    -->  t.vertical
+			t = new Tile(value, right, vertical, left, 
+						 (rotation + 4) % 6);
+		}
 		return t;
 	}
 	
@@ -179,64 +220,92 @@ public class Tile {
 		}
 		return null;
 	}
-	
-	 public String toString() {
+
+	public String toString() {
 	    return "" + stringTile().charAt(1) + stringTile().charAt(2) + stringTile().charAt(3) + stringTile().charAt(4);
 	 }
 	 
-	 
 	// =================== Visual Representations ===================
-	/*
-	 * @ requires this.rotation % 2 == 0;
+	 
+	/**
+	 * Show a visual representation of a Tile.
 	 */
 	public void showTile() {
-		String template =" " +
-                "        / \\\n" +
-                "        / " + value + " \\\n" +
-                "       /"+ left+ " "  + " " +right +" \\\n" +
-                "      /   " + vertical +"   \\\n" +
-                "     /---------\\\n" ;
-		System.out.print(template);
-	}
-	
-	public void showTiledown() {
-		String template = 
-			     "   \\---------/\n" +
-			     "     \\ " + "  " + vertical + "   " +"/ \n" +
-                 "      \\" + left + " "  + " " + right +  " "+ "/\n" +
-                 "       \\ " + value + " "+ "/\n" +
-			     "        \\ / \n " ;
-		System.out.print(template);
-	}
-	
-	
-	public void showTileRotations() {
-		if (this.getRotation() % 2 == 0) {
-			for (int i = 0; i < 3; i++) {
-				this.showTile();
-				this.rotateTile();
-			}
+		if (isFacingUp()) {
+			showTileUp();
 		} else {
-			for (int i = 0; i < 3; i++) {
-				this.showTiledown();
-				this.rotateTile();
-			}
+			showTileDown();
 		}
 	}
+	 
+	/**
+	 * Helper function of showTile(). 
+	 */
+	private void showTileUp() {
+		String template =
+				// make sure each line is of the same total length
+				// for GameTUI to display 3 tiles horizontally
+                "    / \\    \n" +
+                "   / " + value + " \\   \n" +
+                "  / " + left + " " + right + " \\  \n" +
+                " /   " + vertical + "   \\ \n" +
+                " --------- \n"  ;
+		
+		System.out.println(template);
+	}
+	
+	
+	/**
+	 * Helper function of showTile().
+	 */
+	private void showTileDown() {
+		String template = 
+				// make sure each line is of the same total length
+				// for GameTUI to display 3 tiles horizontally
+			     " --------- \n" +
+			     " \\   " + vertical + "   / \n" +
+                 "  \\ " + left + " " + right + " /  \n" +
+                 "   \\ " + value + " /   \n" +
+			     "    \\ /    \n" ;
+		System.out.print(template);
+	}
+	
+
+	// Show all up rotation or all down rotation should be the function of GameTUI.
+	// A Tile only has to show itself. Here this method can be removed.
+//	public void showTileRotations() {
+//		if (this.getRotation() % 2 == 0) {
+//			for (int i = 0; i < 3; i++) {
+//				this.showTile();
+//				this.rotateTile();
+//			}
+//		} else {
+//			for (int i = 0; i < 3; i++) {
+//				this.showTileDown();
+//				this.rotateTile();
+//			}
+//		}
+//	}
 	
 	// =================== Main ===================
 	public static void main(String[] args) {
 		Tile t = new Tile(3,"RGB");
-		Tile t1 = new Tile(3, "RGB");
-		//System.out.println(t.stringTile().equals(t1.stringTile()));
-		//t1.showTileRotations();
-		t1.rotateTile();
-		t1.showTileRotations();
-//		t1.showTiledown();
+//		Tile t1 = new Tile(3, "RGB");
+//		System.out.println(t.stringTile().equals(t1.stringTile()));
+//		t1.showTileRotations();
+//		t1.rotateTile();
+//		t1.showTileRotations();
+//		t1.showTileDown();
 //		t1.showTile();
 //		String s = t.stringTile();
 //		System.out.println(s);
 //		System.out.println(t.toString());
+		t.showTile();						// rotation 0
+		t.rotateTileOnce().showTile();		// rotation 1
+		t.rotateTileTwice().showTile();		// rotation 2
+		t.rotateTileOnce().rotateTileTwice().showTile();  		// rotation 3
+		t.rotateTileFourTimes().showTile();;					// rotation 4
+		t.rotateTileOnce().rotateTileFourTimes().showTile();    // rotation 5
 	}
 
 }

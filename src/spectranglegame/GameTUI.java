37531fd6b61409e8b455aa9c4421e7f766f0c697
Ezra <deleguo@gmail.com>
2019-01-29 23:23:10 +0100

    re-write canPlay2(Player p), now every contacting Tile edge has to mach

--- src/spectranglegame/GameTUI.java
+++ src/spectranglegame/GameTUI.java
@@ -29,6 +29,7 @@ public class GameTUI {
     // =========== Instance Field ===========
 	private GameControl control;
 	private Board board;
+	private Bag bag;
 	private Tile chosenBaseTile = null;
 	private Integer chosenFieldIdx = null;
 	private Boolean chosenFieldFacingUp = null;
@@ -36,10 +37,11 @@ public class GameTUI {
 	private List<Player> listPlayers; 
 	
 	// =========== Constructor ===========
-	public GameTUI(GameControl gc, Board bd, List<Player> lp) {
+	public GameTUI(GameControl gc, Board bd, List<Player> lp, Bag bg) {
 		this.control = gc;
 		this.board = bd;
 		this.listPlayers = lp;
+		this.bag = bg;
 	}
 
 	// ========================= Ask User Input =========================
@@ -48,41 +50,76 @@ public class GameTUI {
      * @param p Ask this player for input.
      * @return The Tile (cloned) of the player's choice. 
      */
-    public Tile askTile(Player p, boolean isFirstMove) {
-    	chosenBaseTile = null;
-    	// later this will be sent to p via socket, and p will parse it 
-		System.out.println(TILE); 
+//    public Tile askTile(Player p, boolean isFirstMove) {
+	public Tile askTile(Player p) {
+		chosenBaseTile = null;
 		
-		ArrayList<Tile> nonNulls = getNonNullTiles(p);
+		ArrayList<Tile> nonNulls = p.getNonNullTiles();
 		// Later TUI should send this String representation to PlayerClient
 		// For now TUI print this String at TUI's console.
 		showMultiTilesUp(nonNulls);
-		
-		int nonNullTileIdx = p.chooseTileIdx(nonNulls.size(), isFirstMove);
-		
-		if ((nonNullTileIdx != 8) && (nonNullTileIdx != 9)) {
-			// copy the current chosen Tile in TUI
-			// for TUI to later check normal move sanitary (surrounding sanitary)
-			chosenBaseTile = nonNulls.get(nonNullTileIdx).deepCopy();
+    		
+		if (canPlay(p)) {
+			// make sure user will choose a legal tile
+    		int nonNullTileIdx = p.chooseTileIdx(nonNulls.size());
+    		
+    		// C -> S: Move <index> <tile encoding> (half-way)
+    		chosenBaseTile = nonNulls.get(nonNullTileIdx).deepCopy();
 			
 			return chosenBaseTile;
-			
-		} else if (nonNullTileIdx == 8){
-			
-			return null; // choose no tile, and do nothing else
-			
-		} else { 
-			// when nonNullTileIdx = 9, user wants to exchange a tile with bag 
-			Integer tileIdxToSwap = askTileToSwap(p, nonNulls); 
-			
-			if (tileIdxToSwap != null) {
+    	} 
+    	// (user can't play) && (bag is not empty)
+    	else if (!bagIsEmpty()) {
+    		// user have the choice to skip or swap
+    		Integer tileIdxToSwap = p.chooseSkipOrSwap(nonNulls.size());
+    		
+    		if (tileIdxToSwap == null) {
+    			// C -> S: Skip
+    		} else {
 				Tile fromPlayer = nonNulls.get(tileIdxToSwap);
 				Tile fromBag = control.swapRandomTileInBag(fromPlayer);
 				swapTileAtPlayer(p, fromPlayer, fromBag);
+				// C -> S: Skip <encoding of fromPlayer>
 			}
-			return null;  // and choose no tile
+    		return null;
+    		
+    	}
+    	// (user can't play) && (bag is empty)
+    	else {
+    		// user can only skip
+    		p.toSkip();
+    		// C -> S: Skip
+    		return null;
+    	}
+    }
+    
+    public Integer askTileToSwap(Player p, List<Tile> nonNullAtPlayer) {
+    	
+    	int firstNonNullInBag = control.getFirstNonNullIdx();
+    	
+    	if (firstNonNullInBag < 36) {
+    		int toSwapIdx = p.chooseTileIdxToSwap(nonNullAtPlayer.size());
+    		return toSwapIdx;
+    	} else {
+    		// Later this msg will be send to player via socket
+    		System.out.println("Bag is empty, no tile to swap. Your turn ends.");
+    		return null;
+    	}
+    	
+    }
+  
+    public void swapTileAtPlayer(Player p, Tile toToss, Tile toEquip) {
+		for (int i = 0; i < p.getTiles().length; i++) {
+			if ( (p.getTiles()[i] != null) && 
+					(p.getTiles()[i].toString().equals(toToss.toString())) ) {
+				p.getTiles()[i] = toEquip;
+				break;
+			}
 		}
-		
+		// Later this msg will be sent to Player p via socket
+		// For now print msg to TUI's console.
+		System.out.println("Your tile is swapped!");
+    	
     }
     
     /**
@@ -90,7 +127,7 @@ public class GameTUI {
      * @param p Ask this player for input.
      * @return The index of the player's choice.
      */
-    public Integer askField(Player p, Board b, boolean isFirstMove) {
+    public Integer askField(Player p, Board b) {
     	chosenFieldIdx = null;
     	chosenFieldFacingUp = null;
     	boolean isLegalField = false;
@@ -123,7 +160,7 @@ public class GameTUI {
     		boolean cond1 = !Board.isBonusField(returnVal); 
     		boolean cond2 = board.fieldIsEmpty(returnVal);
     		
-    		isLegalField = (isFirstMove) ? (cond1 && cond2) : (cond0 && cond2);
+    		isLegalField = (isFirstMove()) ? cond1 : (cond0 && cond2);
     		
     		if (isLegalField) {
     			chosenFieldIdx = returnVal;
@@ -145,7 +182,7 @@ public class GameTUI {
      * @param t The tile with rotation 0, as the base Tile
      * @return
      */
-    public Tile askRotation(Player p, Tile baseT, boolean isFirstMove) {
+    public Tile askRotation(Player p, Tile baseT) {
     	Tile chosenRotation = null;
     	
     	// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ Below are printing all possible Rotation  ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
@@ -182,7 +219,7 @@ public class GameTUI {
     	while (true) {
     		chosenRotation = allRotation.get(p.chooseRotationIdx());
     		
-    		if (isFirstMove) {
+    		if (isFirstMove()) {
     			// First Move don't care about neighbors
     			break;
     		}
@@ -218,42 +255,162 @@ public class GameTUI {
     	return chosenRotation;
     }
  
-    public Integer askTileToSwap(Player p, List<Tile> nonNullAtPlayer) {
-    	
-    	int firstNonNullInBag = control.getFirstNonNullIdx();
-    	
-    	if (firstNonNullInBag < 36) {
-    		int toSwapIdx = p.chooseTileIdxToSwap(nonNullAtPlayer.size());
-    		return toSwapIdx;
-    	} else {
-    		// Later this msg will be send to player via socket
-    		System.out.println("Bag is empty, no tile to swap. Your turn ends.");
-    		return null;
-    	}
-    	
-    }
-  
-    public void swapTileAtPlayer(Player p, Tile toToss, Tile toEquip) {
-		for (int i = 0; i < p.getTiles().length; i++) {
-			if ( (p.getTiles()[i] != null) && 
-					(p.getTiles()[i].toString().equals(toToss.toString())) ) {
-				p.getTiles()[i] = toEquip;
-				break;
+    
+    // ========================= Query Model Data =========================
+	/**
+	 * Return true if Player p has a tile that can be placed on board.
+	 */
+	public boolean canPlay(Player p) {
+		// if no Tile on board, and Player has Tile, it's first move, always can play.
+		if (isFirstMove()) {
+			return true;
+		}
+		
+		// if player doesn't have non null Tile, canPlay is false
+		if (p.getNonNullTiles().size() == 0) {
+			return false;
+		}
+		
+		// all boarders open to be matched
+		ArrayList<Character> openToMatch = new ArrayList<>();
+		
+		for (Integer idx : Board.hasVN) {
+			// if there's a Tile on it, and it's vertical boarder color is null
+			// then this Tile's vertical color is open to be matched
+			if ((!board.fieldIsEmpty(idx)) && (board.getVerticalBoarderColor(idx) == null)) {
+				openToMatch.add(board.getTile(idx).getVertical());
 			}
 		}
-		// Later this msg will be sent to Player p via socket
-		// For now print msg to TUI's console.
-		System.out.println("Your tile is swapped!");
-    	
-    }
-    // ========================= Display Tiles Related =========================
+		
+		for (Integer idx : Board.hasLN) {
+			// if there's a Tile on it, and it's left boarder color is null
+			// then this Tile's left color is open to be matched
+			if ((!board.fieldIsEmpty(idx)) && (board.getLeftBoarderColor(idx) == null)) {
+				openToMatch.add(board.getTile(idx).getLeft());
+			}
+		}
+		
+		for (Integer idx : Board.hasRN) {
+			// if there's a Tile on it, and it's right boarder color is null
+			// then this Tile's right color is open to be matched
+			if ((!board.fieldIsEmpty(idx)) && (board.getRightBoarderColor(idx) == null)) {
+				openToMatch.add(board.getTile(idx).getRight());
+			}
+		}
+		
+		ArrayList<Character> atHand = new ArrayList<>();
+		for (Tile t : p.getNonNullTiles()) {
+			atHand.add(t.getVertical());
+			atHand.add(t.getLeft());
+			atHand.add(t.getRight());
+		}
+		
+		// only leave the intersection of atHand and openToMath in atHand 
+		atHand.retainAll(openToMatch); 
+		
+		return atHand.size() > 0;
+	}
+
+	public boolean canPlay2(Player p) {
+		// if no Tile on board, and Player has Tile, it's first move, always can play.
+		if (isFirstMove()) {
+			return true;
+		}
+		
+		// if player doesn't have non null Tile, canPlay is false
+		if (p.getNonNullTiles().size() == 0) {
+			return false;
+		}
+		
+		// when there's already some Tile on board, and Player has Tile in hand
+		ArrayList<Integer> emptyWNT = board.getEmptyFieldsWithNeighborTile();
+		// for every empty field with at least a neighboring Tile
+		for (Integer idx : emptyWNT) {
+			Character[] srd = board.getSurroundingInfo(idx);
+			
+			// for every tile in user's hand
+			for (Tile t : p.getNonNullTiles()) {
+				// for every tile in rotation 0, 2, 4, return true as long as you find a match
+				if (srd[0] == 'U') {
+					ArrayList<Tile> upRotations = new ArrayList<Tile>(
+							Arrays.asList(t, t.rotateTileTwice(), t.rotateTileFourTimes()));
+					for (Tile up : upRotations) {
+						boolean match = boarderMatchs(srd, up);
+						if (match) {
+							return true;
+						}
+					}	
+					
+				}
+				// for every tile in rotation 1, 3, 5, return true as long as you find a match
+				else {
+					ArrayList<Tile> downRotations = new ArrayList<Tile>(
+							Arrays.asList(t.rotateTileOnce(), t.rotateTileOnce().rotateTileTwice(), t.rotateTileOnce().rotateTileFourTimes()));
+					for (Tile down : downRotations) {
+						boolean match = boarderMatchs(srd, down);
+						if (match) {
+							return true;
+						}
+					}	
+				}
+				
+				
+			}	
+		}
+		return false;
+	}
+	
+	/**
+	 * Given the surrounding infos of an empty field with at least one neighboring tile, and a specific Tile, 
+	 * can the Tile be placed on the field?
+	 * @param srds The surrounding information of the field.
+	 * @param t The Tile you want to place on the empty field.
+	 * @return true If the tile match the surroundings.
+	 */
+	/*
+	 * @requires (srd[1] != null) || (srd[2] != null) || (srd[3] != null);
+	 * @requires ((srd[0] == 'U') && (t.isFacingUp())) || ((srd[0] == 'D') && (!t.isFacingUp()));
+	 */
+	public boolean boarderMatchs(Character[] srd, Tile t) {
+		// (has a vertical neighbor) && (vertical color mismatch)
+		if ((srd[1] != null) && (t.getVertical() != srd[1])) {
+			return false;
+		}
+		
+		// (has a left neighbor) && (left color mismatch)
+		if ((srd[2] != null) && (t.getLeft() != srd[2])) {
+			return false;
+		}
+		
+		// (has a right neighbor) && (right color mismatch)
+		if ((srd[3] != null) && (t.getRight() != srd[3])) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean bagIsEmpty() {
+		for (Tile t : bag.getTiles()) {
+			if (t != null) {
+				return false;
+			}
+		}
+		return true;
+	}
+    
+	public boolean isFirstMove() {
+		return board.getEmptyFields().size() == 36;
+	}
+	
+	// ========================= Display Tiles Related =========================
     // Later this whole bunch of string will be sent to PlayerClient via socket
     // For now TUI print to TUI's console.
     public void showInfoToPlayer(Player sendToThisPlayer) {
     	// show each Player's Tile 
     	for (Player p : listPlayers) {
     		System.out.println("Player " + p.getName() + "'s Tiles: ");
-    		ArrayList<Tile> nonNulls = getNonNullTiles(p);
+    		ArrayList<Tile> nonNulls = p.getNonNullTiles();
     		// Later TUI should send this String representation to PlayerClient
     		// For now TUI print this String at TUI's console.
     		showMultiTilesUp(nonNulls);
@@ -261,19 +418,6 @@ public class GameTUI {
     	
     	// show board
     	printBoardDynamic(board);
-    }
-    
-    private ArrayList<Tile> getNonNullTiles(Player p){
-    	Tile[] tilesArray =  p.getTiles();
-    	
-    	// get non-null Tile and store them in a ArrayList (such that there's no null in between)
-		ArrayList<Tile> nonNullTiles = new ArrayList<>();
-		for (int i = 0; i < tilesArray.length; i++) {
-			if (tilesArray[i] != null) {
-				nonNullTiles.add(tilesArray[i]);
-			}
-		}
-		return nonNullTiles;
     }
     
     private void showMultiTilesUp(ArrayList<Tile> nonNullTiles) {
@@ -513,27 +657,27 @@ public class GameTUI {
 	
     // ========================= Main =========================
     public static void main(String[] args) {
-    	Tile[] tiles = {new Tile(3, "RGB")};
-    	List<Player> pl = Arrays.asList(new HumanPlayer("player", tiles));
-    	
-		GameTUI temp = new GameTUI(new GameControl(pl, true), 
-								   new Board(), pl);
-		
-		// test of showTile
-//		temp.showThreeTilesUp(Arrays.asList(new Tile(3, "RGB"), new Tile(1, "HEY"), new Tile(2, "OOP")));
-//		temp.showThreeTilesDown(Arrays.asList(new Tile(3, "RGB"), new Tile(1, "HEY"), new Tile(2, "OOP")));
-		
-		// test of askField, askTileAndRotation:
-		Tile[] tiles1 = {new Tile(3, "RGB"), null, null, null};
-		Tile[] tiles2 = {new Tile(3, "RGB"), null, new Tile(1, "HEY"), null};
-		Tile[] tiles3 = {new Tile(3, "RGB"), new Tile(1, "HEY"), null, new Tile(2, "OOP")};
-		Tile[] tiles4 = {new Tile(3, "RGB"), new Tile(1, "HEY"), new Tile(2, "OOP"), new Tile(4, "WHO")};
-		
-		HumanPlayer p = new HumanPlayer("player", tiles2);
-		Tile t0 = temp.askTile(p, true);
-		int i = temp.askField(p, new Board(), true);
-		Tile t1 = temp.askRotation(p, t0, true);
-		
+//    	Tile[] tiles = {new Tile(3, "RGB")};
+//    	List<Player> pl = Arrays.asList(new HumanPlayer("player", tiles));
+//    	
+//		GameTUI temp = new GameTUI(new GameControl(pl, true), 
+//								   new Board(), pl);
+//		
+//		// test of showTile
+////		temp.showThreeTilesUp(Arrays.asList(new Tile(3, "RGB"), new Tile(1, "HEY"), new Tile(2, "OOP")));
+////		temp.showThreeTilesDown(Arrays.asList(new Tile(3, "RGB"), new Tile(1, "HEY"), new Tile(2, "OOP")));
+//		
+//		// test of askField, askTileAndRotation:
+//		Tile[] tiles1 = {new Tile(3, "RGB"), null, null, null};
+//		Tile[] tiles2 = {new Tile(3, "RGB"), null, new Tile(1, "HEY"), null};
+//		Tile[] tiles3 = {new Tile(3, "RGB"), new Tile(1, "HEY"), null, new Tile(2, "OOP")};
+//		Tile[] tiles4 = {new Tile(3, "RGB"), new Tile(1, "HEY"), new Tile(2, "OOP"), new Tile(4, "WHO")};
+//		
+//		HumanPlayer p = new HumanPlayer("player", tiles2);
+//		Tile t0 = temp.askTile(p, true);
+//		int i = temp.askField(p, new Board(), true);
+//		Tile t1 = temp.askRotation(p, t0, true);
+//		
 		
 		
 	}
